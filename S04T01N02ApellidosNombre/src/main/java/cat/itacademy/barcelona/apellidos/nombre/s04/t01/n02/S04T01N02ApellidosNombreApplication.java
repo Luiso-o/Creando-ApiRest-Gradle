255c484fdc48fe202261a6368e898bbf87dd9a18
luiso-o <cheportillo@gmail.com>
2023-07-04 16:26:12 +0200

    "Primera Versión"

---
+++ S04T01N02ApellidosNombre/src/main/java/cat/itacademy/barcelona/apellidos/nombre/s04/t01/n02/S04T01N02ApellidosNombreApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelona.apellidos.nombre.s04.t01.n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02ApellidosNombreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02ApellidosNombreApplication.class, args);
+	}
+
+}
